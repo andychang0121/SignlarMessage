eebdfff43d2d1b8b18ebe3a384dd31f12e5b3d22
安迪學長 <andy_chang_0121@hotmail.com>
2016-12-30 21:30:35 +0800

    加入專案檔案。

---
+++ SignlarMessage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignlarMessage.MvcApplication" Language="C#" %>
---
+++ SignlarMessage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignlarMessage.Startup))]
+namespace SignlarMessage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
